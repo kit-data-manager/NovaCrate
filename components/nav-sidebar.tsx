389f6980912074a87887c16cb457e51f12471b5c
Christopher Raquet <uitxy@student.kit.edu>
2024-03-25 12:53:07 +0100

    Theming

--- components/nav-sidebar.tsx
+++ components/nav-sidebar.tsx
@@ -1,9 +1,22 @@
+"use client"
+
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 import { Button } from "@/components/ui/button"
-import { Bug, Cog, Folder, GitFork, Package, PackageSearch, User } from "lucide-react"
-import { PropsWithChildren } from "react"
+import { Bug, Cog, Folder, GitFork, Moon, Package, PackageSearch, Sun, User } from "lucide-react"
+import { PropsWithChildren, useCallback } from "react"
+import { useTheme } from "next-themes"
 
 export function NavSidebar({ children }: PropsWithChildren<{}>) {
+    const theme = useTheme()
+
+    const toggleTheme = useCallback(() => {
+        if (theme.theme === "light") {
+            theme.setTheme("dark")
+        } else {
+            theme.setTheme("light")
+        }
+    }, [theme])
+
     return (
         <ResizablePanelGroup direction="horizontal" autoSaveId="globalSidebarLayout">
             <ResizablePanel minSize={10} defaultSize={12}>
@@ -31,6 +44,18 @@ export function NavSidebar({ children }: PropsWithChildren<{}>) {
 
                     <div className="grow"></div>
 
+                    <Button
+                        onClick={toggleTheme}
+                        variant="link"
+                        className="justify-start justify-self-end"
+                    >
+                        {theme.theme === "light" ? (
+                            <Sun className="h-4 w-4 mr-2" />
+                        ) : (
+                            <Moon className="h-4 w-4 mr-2" />
+                        )}
+                        {theme.theme === "light" ? "Light Theme" : "Dark Theme"}
+                    </Button>
                     <Button variant="link" className="justify-start justify-self-end">
                         <User className="h-4 w-4 mr-2" />
                         Login
