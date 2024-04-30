9612240b6522b7cba9b6de799376bebc8659857f
Christopher Raquet <uitxy@student.kit.edu>
2024-04-30 18:28:15 +0200

    Start work on better server sync

--- components/ensure-sync.tsx
+++ components/ensure-sync.tsx
@@ -57,3 +57,63 @@ export function executeForcedUpdates(
         }
     }
 }
+
+// TODO use these to properly compare old crate data and new crate data
+function compareRecords<A extends Record<string, unknown>, B extends Record<string, unknown>>(
+    a: A,
+    b: B
+) {
+    const onlyInA: string[] = []
+    const onlyInB: string[] = []
+    const shared: string[] = []
+
+    Object.keys(a)
+        .filter((key) => !Object.keys(b).includes(key))
+        .forEach((key) => onlyInA.push(key))
+    Object.keys(b)
+        .filter((key) => !Object.keys(a).includes(key))
+        .forEach((key) => onlyInB.push(key))
+    Object.keys(a)
+        .filter((key) => Object.keys(b).includes(key))
+        .forEach((key) => shared.push(key))
+
+    function handleOnlyInA(cb: (key: string) => void) {
+        onlyInA.forEach(cb)
+    }
+
+    function handleOnlyInB(cb: (key: string) => void) {
+        onlyInB.forEach(cb)
+    }
+
+    function handleShared(cb: (key: string) => void) {
+        shared.forEach(cb)
+    }
+
+    return { handleOnlyInA, handleOnlyInB, handleShared }
+}
+
+function compareEntities(a: IFlatEntity, b: IFlatEntity) {
+    const { handleOnlyInA, handleOnlyInB, handleShared } = compareRecords(a, b)
+    const unchanged: string[] = []
+    const changed: string[] = []
+
+    handleShared((key) => {
+        const propA = a[key]
+        const propB = b[key]
+        if (propertyHasChanged(propA, propB)) {
+            changed.push(key)
+        } else {
+            unchanged.push(key)
+        }
+    })
+
+    function handleUnchanged(cb: (key: string) => void) {
+        unchanged.forEach(cb)
+    }
+
+    function handleChanged(cb: (key: string) => void) {
+        changed.forEach(cb)
+    }
+
+    return { handleOnlyInA, handleOnlyInB, handleChanged, handleUnchanged }
+}
