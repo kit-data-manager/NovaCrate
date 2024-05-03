fb126fe0c1d850798d1cceb1d2ed5582bef8f2b6
Christopher Raquet <uitxy@student.kit.edu>
2024-05-03 13:04:00 +0200

    Style improvements

--- components/editor/id-field.tsx
+++ components/editor/id-field.tsx
@@ -1,18 +1,10 @@
-import { ChangeEvent } from "react"
-import { Button } from "@/components/ui/button"
-import { Fingerprint } from "lucide-react"
+import { ScanBarcode } from "lucide-react"
 
-export function IDField({
-    value,
-    onChange
-}: {
-    value: string
-    onChange: (value: ChangeEvent<HTMLInputElement>) => void
-}) {
+export function IDField({ value }: { value: string }) {
     return (
-        <Button variant="outline" className="flex grow justify-start pl-3">
-            <Fingerprint className="w-4 h-4 pointer-events-none text-muted-foreground mr-2" />
+        <div className="flex grow justify-start pl-3 items-center rounded-lg p-2">
+            <ScanBarcode className="w-4 h-4 pointer-events-none text-muted-foreground mr-2" />
             {value}
-        </Button>
+        </div>
     )
 }
