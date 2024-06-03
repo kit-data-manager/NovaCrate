26864415151e2f21e6181b14d52e8d0ec270f9b3
Christopher Raquet <uitxy@student.kit.edu>
2024-06-03 15:34:51 +0200

    Stark work on correct date format

--- components/editor/text-fields/datetime-field.tsx
+++ components/editor/text-fields/datetime-field.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, memo, useCallback } from "react"
+import { ChangeEvent, memo, useCallback, useMemo } from "react"
 import { Input } from "@/components/ui/input"
 import { CalendarClock, Clock9, TypeIcon } from "lucide-react"
 import { SinglePropertyDropdown } from "@/components/editor/single-property-dropdown"
 import { SlimClass } from "@/lib/crate-verify/helpers"
 import { PropertyEditorTypes } from "@/components/editor/property-editor"
+import { DateTime } from "luxon"
 
 export const DateTimeField = memo(function DateTimeField({
     value,
@@ -20,16 +21,22 @@ export const DateTimeField = memo(function DateTimeField({
 }) {
     const onInputChange = useCallback(
         (e: ChangeEvent<HTMLInputElement>) => {
-            onChange(e.target.value)
+            onChange(DateTime.fromISO(e.target.value).toISO() || e.target.value)
         },
         [onChange]
     )
+
+    const parsedValue = useMemo(() => {
+        const txt = DateTime.fromISO(value).toLocal().toISO()
+        console.log(txt)
+        return txt
+    }, [])
 
     return (
         <div className="flex w-full relative">
             <CalendarClock className="w-4 h-4 absolute left-2.5 top-3 pointer-events-none text-muted-foreground" />
             <Input
-                value={value}
+                value={parsedValue || value}
                 onChange={onInputChange}
                 className="self-center rounded-r-none pl-9"
                 type="datetime-local"
