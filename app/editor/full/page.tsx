a4251f76da751dd8194ce368c23e63bd81b55de2
Christopher Raquet <uitxy@student.kit.edu>
2024-06-09 22:49:30 +0200

    Automatically close editor when crate id unknown

--- app/editor/full/page.tsx
+++ app/editor/full/page.tsx
@@ -4,7 +4,7 @@ import { AutoRedirect } from "@/components/auto-redirect"
 export default function Home() {
     return (
         <Link href={"entities"}>
-            View Entities <AutoRedirect />
+            <AutoRedirect />
         </Link>
     )
 }
