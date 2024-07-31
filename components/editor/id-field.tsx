df2f90d4625989594e9ccdca37a3545bfe1c69f3
Christopher Raquet <uitxy@student.kit.edu>
2024-07-31 13:57:36 +0200

    Add preview button to ID field

--- components/editor/id-field.tsx
+++ components/editor/id-field.tsx
@@ -1,4 +1,4 @@
-import { Copy, EllipsisVertical, ScanBarcode } from "lucide-react"
+import { Copy, EllipsisVertical, Eye, ScanBarcode } from "lucide-react"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -6,10 +6,36 @@ import {
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu"
 import { useCopyToClipboard } from "usehooks-ts"
-import { useCallback } from "react"
+import { useCallback, useMemo } from "react"
+import { useEditorState } from "@/lib/state/editor-state"
+import { canHavePreview as canHavePreviewUtil } from "@/lib/utils"
+import { useEntityEditorTabs } from "@/lib/state/entity-editor-tabs-state"
 
 export function IDField({ value }: { value: string }) {
     const [_, copy] = useCopyToClipboard()
+    const type = useEditorState((state) => state.entities.get(value)?.["@type"])
+    const previewingFilePath = useEntityEditorTabs((store) => store.previewingFilePath)
+    const setPreviewingFilePath = useEntityEditorTabs((store) => store.setPreviewingFilePath)
+
+    const canHavePreview = useMemo(() => {
+        return type
+            ? canHavePreviewUtil({
+                  // Dirty hack but should be perfectly fine, since we have no access to the full entity here
+                  "@id": value,
+                  "@type": type
+              })
+            : false
+    }, [type, value])
+
+    const togglePreview = useCallback(() => {
+        if (type) {
+            if (previewingFilePath === value) {
+                setPreviewingFilePath("")
+            } else {
+                setPreviewingFilePath(value)
+            }
+        }
+    }, [previewingFilePath, setPreviewingFilePath, type, value])
 
     const copyFn = useCallback(() => {
         copy(value).then()
@@ -24,8 +50,13 @@ export function IDField({ value }: { value: string }) {
                     <EllipsisVertical className="w-4 h-4" />
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
+                    {canHavePreview ? (
+                        <DropdownMenuItem onClick={togglePreview}>
+                            <Eye className="w-4 h-4 mr-2" /> Preview File
+                        </DropdownMenuItem>
+                    ) : null}
                     <DropdownMenuItem onClick={copyFn}>
-                        <Copy className="w-4 h-4 mr-2" /> Copy
+                        <Copy className="w-4 h-4 mr-2" /> Copy Identifier
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
