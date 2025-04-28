14675da1a4106409c7ff040625465ef2ac3e955b
Christopher Raquet <xf3778@kit.edu>
2025-04-28 15:25:41 +0200

    Styling fixes

--- components/ui/menubar.tsx
+++ components/ui/menubar.tsx
@@ -202,7 +202,7 @@ function MenubarSubTrigger({
             data-slot="menubar-sub-trigger"
             data-inset={inset}
             className={cn(
-                "focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex cursor-default items-center rounded-sm px-2 py-1.5 text-sm outline-none select-none data-[inset]:pl-8 gap-2",
+                "focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex cursor-default items-center rounded-sm px-2 py-1.5 text-sm outline-none select-none data-[inset]:pl-8",
                 className
             )}
             {...props}
