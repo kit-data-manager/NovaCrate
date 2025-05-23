44a6a280ac2c76a888a1ecbf52d1f2681509fd1f
Christopher Raquet <christopher.raquet@kit.edu>
2025-05-23 16:42:17 +0200

    Implement entity renaming, also renames single referenced file or directory

--- components/editor/id-field.tsx
+++ components/editor/id-field.tsx
@@ -1,4 +1,4 @@
-import { Copy, EllipsisVertical, Eye, ScanBarcode } from "lucide-react"
+import { Copy, EllipsisVertical, Eye, Pencil, ScanBarcode } from "lucide-react"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -6,16 +6,19 @@ import {
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu"
 import { useCopyToClipboard } from "usehooks-ts"
-import { useCallback, useMemo } from "react"
+import { useCallback, useMemo, useState } from "react"
 import { useEditorState } from "@/lib/state/editor-state"
 import { canHavePreview as canHavePreviewUtil } from "@/lib/utils"
 import { useEntityEditorTabs } from "@/lib/state/entity-editor-tabs-state"
+import { Button } from "@/components/ui/button"
+import { RenameEntityModal } from "@/components/modals/rename-entity-modal"
 
 export function IDField({ value }: { value: string }) {
-    const [_, copy] = useCopyToClipboard()
+    const [, copy] = useCopyToClipboard()
     const type = useEditorState((state) => state.entities.get(value)?.["@type"])
     const previewingFilePath = useEntityEditorTabs((store) => store.previewingFilePath)
     const setPreviewingFilePath = useEntityEditorTabs((store) => store.setPreviewingFilePath)
+    const [renameEntityModalOpen, setRenameEntityModalOpen] = useState(false)
 
     const canHavePreview = useMemo(() => {
         return type
@@ -42,14 +45,25 @@ export function IDField({ value }: { value: string }) {
     }, [copy, value])
 
     return (
-        <div className="flex grow justify-start pl-3 items-center rounded-lg p-2">
+        <div className="flex grow justify-start pl-3 items-center rounded-lg p-2 pr-0">
+            <RenameEntityModal
+                entityId={value}
+                onOpenChange={setRenameEntityModalOpen}
+                open={renameEntityModalOpen}
+            />
+
             <ScanBarcode className="size-4 pointer-events-none text-muted-foreground mr-2 shrink-0" />
             <span className="truncate grow">{value}</span>
             <DropdownMenu>
-                <DropdownMenuTrigger className="p-2">
-                    <EllipsisVertical className="size-4" />
+                <DropdownMenuTrigger className="p-2" asChild>
+                    <Button variant={"outline"}>
+                        <EllipsisVertical className="size-4" />
+                    </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
+                    <DropdownMenuItem onClick={() => setRenameEntityModalOpen(true)}>
+                        <Pencil className="size-4 mr-2" /> Edit
+                    </DropdownMenuItem>
                     {canHavePreview ? (
                         <DropdownMenuItem onClick={togglePreview}>
                             <Eye className="size-4 mr-2" /> Preview File
