e19a2c2aefad6836610b436bb76552f3652f0668
Christopher Raquet <christopher.raquet@kit.edu>
2025-05-23 13:42:51 +0200

    Text fields can now be expanded to textarea for easy editing of longer texts

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,10 +21,11 @@ export default defineConfig({
             use: { ...devices["Desktop Chrome"] }
         }
     ],
-    webServer: {
-        command: `npm run dev`,
-        url: "http://localhost:3000/",
-        timeout: 120 * 1000,
-        reuseExistingServer: !process.env.CI
-    }
+    webServer: process.env.CI
+        ? {
+              command: `npm run serve`,
+              url: "http://localhost:3000/",
+              timeout: 120 * 1000
+          }
+        : undefined
 })
