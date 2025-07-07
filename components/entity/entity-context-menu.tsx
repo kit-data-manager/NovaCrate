36d1e1215fa976b7215986cfc90c29630ec0cedf
Christopher Raquet <christopher.raquet@kit.edu>
2025-07-07 16:11:32 +0200

    Add context menu to graph nodes

--- components/entity/entity-context-menu.tsx
+++ components/entity/entity-context-menu.tsx
@@ -7,24 +7,25 @@ import {
 } from "@/components/ui/context-menu"
 import { EntityIcon } from "@/components/entity/entity-icon"
 import { Diff, isFileDataEntity } from "@/lib/utils"
-import { Eye, Save, Trash, Undo2 } from "lucide-react"
+import { Eye, Folder, Save, Trash, Undo2 } from "lucide-react"
 import { PropsWithChildren, useCallback, useContext, useMemo } from "react"
 import { createEntityEditorTab, useEntityEditorTabs } from "@/lib/state/entity-editor-tabs-state"
 import { useEditorState } from "@/lib/state/editor-state"
 import { CrateDataContext } from "@/components/providers/crate-data-provider"
 import { GlobalModalContext } from "@/components/providers/global-modals-provider"
+import { useGoToFileExplorer } from "@/lib/hooks"
 
 export function EntityContextMenu({
     entity,
     children,
     asChild
-}: PropsWithChildren<{ entity: IEntity; asChild: boolean }>) {
+}: PropsWithChildren<{ entity?: IEntity; asChild: boolean }>) {
     const openTab = useEntityEditorTabs((store) => store.openTab)
     const setPreviewingFilePath = useEntityEditorTabs((store) => store.setPreviewingFilePath)
     const { saveEntity } = useContext(CrateDataContext)
     const revertEntity = useEditorState((store) => store.revertEntity)
     const { showDeleteEntityModal } = useContext(GlobalModalContext)
-    const diff = useEditorState((state) => state.getEntityDiff(entity["@id"]))
+    const diff = useEditorState((state) => (entity ? state.getEntityDiff(entity["@id"]) : null))
 
     const canHavePreview = useMemo(() => {
         return entity ? isFileDataEntity(entity) : false
@@ -55,6 +56,8 @@ export function EntityContextMenu({
         if (entity) setPreviewingFilePath(entity["@id"])
     }, [entity, setPreviewingFilePath])
 
+    const showInFileExplorer = useGoToFileExplorer(entity)
+
     return (
         <ContextMenu>
             <ContextMenuTrigger asChild={asChild}>{children}</ContextMenuTrigger>
@@ -63,9 +66,15 @@ export function EntityContextMenu({
                     <EntityIcon entity={entity} size="sm" /> Open Tab
                 </ContextMenuItem>
                 {canHavePreview ? (
-                    <ContextMenuItem onClick={onPreviewClick}>
-                        <Eye className="size-4 mr-2" /> Preview
-                    </ContextMenuItem>
+                    <>
+                        <ContextMenuSeparator />
+                        <ContextMenuItem onClick={onPreviewClick}>
+                            <Eye className="size-4 mr-2" /> Preview File
+                        </ContextMenuItem>
+                        <ContextMenuItem onClick={() => showInFileExplorer()}>
+                            <Folder className="size-4 mr-2" /> Show in File Explorer
+                        </ContextMenuItem>
+                    </>
                 ) : null}
                 <ContextMenuSeparator />
                 <ContextMenuItem onClick={onSaveClick} disabled={!hasUnsavedChanges}>
--- components/graph/entity-node.tsx
+++ components/graph/entity-node.tsx
@@ -1,22 +1,12 @@
 import { Handle, Position } from "reactflow"
-import React, { useContext, useMemo } from "react"
+import React, { useMemo } from "react"
 import { camelCaseReadable, Diff, getEntityDisplayName, toArray } from "@/lib/utils"
 import { EntityIcon } from "@/components/entity/entity-icon"
-import { Delete, Plus, Save, Trash, Undo2 } from "lucide-react"
-import {
-    ContextMenu,
-    ContextMenuContent,
-    ContextMenuItem,
-    ContextMenuSeparator,
-    ContextMenuShortcut,
-    ContextMenuTrigger
-} from "@/components/ui/context-menu"
+import { Plus } from "lucide-react"
 import { useEditorState } from "@/lib/state/editor-state"
-import { useGoToEntityEditor } from "@/lib/hooks"
-import { CrateDataContext } from "@/components/providers/crate-data-provider"
-import { GlobalModalContext } from "@/components/providers/global-modals-provider"
 import { useGraphSettings } from "@/components/providers/graph-settings-provider"
 import { useShallow } from "zustand/react/shallow"
+import { EntityContextMenu } from "@/components/entity/entity-context-menu"
 
 export const NEW_PROP_HANDLE = "__special__newProp"
 
@@ -47,14 +37,9 @@ export default function EntityNode({
     const entityHasChanges = useEditorState(
         useShallow((store) => store.getEntitiesChangelist().get(data.entityId) !== Diff.None)
     )
-    const revertEntity = useEditorState((store) => store.revertEntity)
-    const { saveEntity } = useContext(CrateDataContext)
-    const { showDeleteEntityModal } = useContext(GlobalModalContext)
 
     const aggregateProperties = useGraphSettings((store) => store.aggregateProperties)
     const showTextProperties = useGraphSettings((store) => store.showTextProperties)
-
-    const goToEntity = useGoToEntityEditor(entity)
 
     const handles = useMemo(() => {
         if (showTextProperties) {
@@ -65,116 +50,81 @@ export default function EntityNode({
     }, [data.handles, showTextProperties])
 
     return (
-        <ContextMenu>
-            <ContextMenuTrigger>
-                <Handle
-                    type="target"
-                    position={Position.Left}
-                    onConnect={(params) => console.log("handle onConnect", params)}
-                    isConnectable={isConnectable}
-                    className="border-none! bg-primary/80!"
-                />
-                <div
-                    className={`p-3 rounded-lg border dark:border-accent bg-background max-w-[600px] ${selected ? "bg-secondary" : ""}`}
-                >
-                    <div className="flex gap-2 items-center">
-                        <EntityIcon entity={entity} size="lg" unsavedChanges={entityHasChanges} />
+        <EntityContextMenu entity={entity} asChild={false}>
+            <Handle
+                type="target"
+                position={Position.Left}
+                onConnect={(params) => console.log("handle onConnect", params)}
+                isConnectable={isConnectable}
+                className="border-none! bg-primary/80!"
+            />
+            <div
+                className={`p-3 rounded-lg border dark:border-accent bg-background max-w-[600px] ${selected ? "bg-secondary" : ""}`}
+            >
+                <div className="flex gap-2 items-center">
+                    <EntityIcon entity={entity} size="lg" unsavedChanges={entityHasChanges} />
 
-                        <div className="flex flex-col truncate">
-                            <div className={`truncate text-sm ${entity ? "" : "text-root"}`}>
-                                {entity ? getEntityDisplayName(entity) : "Unknown Entity"}
-                                {entity ? (
-                                    <span className="text-muted-foreground text-xs truncate ml-1">
-                                        {toArray(entity["@type"]).join(", ")}
-                                    </span>
-                                ) : null}
-                            </div>
-                            <span className="text-muted-foreground text-xs truncate">{id}</span>
+                    <div className="flex flex-col truncate">
+                        <div className={`truncate text-sm ${entity ? "" : "text-root"}`}>
+                            {entity ? getEntityDisplayName(entity) : "Unknown Entity"}
+                            {entity ? (
+                                <span className="text-muted-foreground text-xs truncate ml-1">
+                                    {toArray(entity["@type"]).join(", ")}
+                                </span>
+                            ) : null}
                         </div>
-
-                        {aggregateProperties ? null : (
-                            <div className="flex flex-col gap-1 ml-2 text-right">
-                                {handles.map((h) => {
-                                    return (
-                                        <div key={h.id} className="text-xs">
-                                            {camelCaseReadable(h.name)}
-                                        </div>
-                                    )
-                                })}
-
-                                <div
-                                    className={`text-muted-foreground text-xs flex items-center justify-end`}
-                                >
-                                    <Plus className="w-3 h-3 mt-1" />
-                                </div>
-                            </div>
-                        )}
+                        <span className="text-muted-foreground text-xs truncate">{id}</span>
                     </div>
+
+                    {aggregateProperties ? null : (
+                        <div className="flex flex-col gap-1 ml-2 text-right">
+                            {handles.map((h) => {
+                                return (
+                                    <div key={h.id} className="text-xs">
+                                        {camelCaseReadable(h.name)}
+                                    </div>
+                                )
+                            })}
+
+                            <div
+                                className={`text-muted-foreground text-xs flex items-center justify-end`}
+                            >
+                                <Plus className="w-3 h-3 mt-1" />
+                            </div>
+                        </div>
+                    )}
                 </div>
-                {aggregateProperties
-                    ? null
-                    : handles.map((h, i) => {
-                          if (h.text) return null
-                          return (
-                              <Handle
-                                  key={h.id}
-                                  id={h.id}
-                                  type="source"
-                                  position={Position.Right}
-                                  style={{ top: handlePos(i, handles.length) }}
-                                  isConnectable={isConnectable}
-                                  className="border-none! bg-primary/80!"
-                              />
-                          )
-                      })}
-                <Handle
-                    id={NEW_PROP_HANDLE}
-                    type="source"
-                    position={Position.Right}
-                    style={
-                        aggregateProperties
-                            ? { right: -2 }
-                            : {
-                                  top: handlePos(handles.length, handles.length)
-                              }
-                    }
-                    isConnectable={isConnectable}
-                    className="border-none! bg-primary/80!"
-                />
-            </ContextMenuTrigger>
-            <ContextMenuContent>
-                {entity ? (
-                    <>
-                        <ContextMenuItem onClick={() => goToEntity()}>
-                            <EntityIcon entity={entity} size="sm" /> Open in Entity Editor
-                        </ContextMenuItem>
-                        <ContextMenuSeparator />
-                        <ContextMenuItem
-                            onClick={() => saveEntity(entity)}
-                            disabled={!entityHasChanges}
-                        >
-                            <Save className="size-4 mr-2" /> Save Changes
-                        </ContextMenuItem>
-                        <ContextMenuItem
-                            onClick={() => revertEntity(data.entityId)}
-                            disabled={!entityHasChanges}
-                        >
-                            <Undo2 className="size-4 mr-2" /> Revert Changes
-                        </ContextMenuItem>
-                        <ContextMenuSeparator />
-                        <ContextMenuItem
-                            variant={"destructive"}
-                            onClick={() => showDeleteEntityModal(data.entityId)}
-                        >
-                            <Trash className="size-4 mr-2" />
-                            Delete
-                            <ContextMenuShortcut>
-                                <Delete className="size-4 text-destructive-foreground/80" />
-                            </ContextMenuShortcut>
-                        </ContextMenuItem>
-                    </>
-                ) : null}
-            </ContextMenuContent>
-        </ContextMenu>
+            </div>
+            {aggregateProperties
+                ? null
+                : handles.map((h, i) => {
+                      if (h.text) return null
+                      return (
+                          <Handle
+                              key={h.id}
+                              id={h.id}
+                              type="source"
+                              position={Position.Right}
+                              style={{ top: handlePos(i, handles.length) }}
+                              isConnectable={isConnectable}
+                              className="border-none! bg-primary/80!"
+                          />
+                      )
+                  })}
+            <Handle
+                id={NEW_PROP_HANDLE}
+                type="source"
+                position={Position.Right}
+                style={
+                    aggregateProperties
+                        ? { right: -2 }
+                        : {
+                              top: handlePos(handles.length, handles.length)
+                          }
+                }
+                isConnectable={isConnectable}
+                className="border-none! bg-primary/80!"
+            />
+        </EntityContextMenu>
     )
 }
