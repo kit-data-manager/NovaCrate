4b493a57a95f0a9c1f2bd72a2fe2b7a11ed42b6a
Christopher Raquet <christopher.raquet@kit.edu>
2025-10-28 16:35:44 +0100

    bump dependencies, update to next.js 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,16 @@
-import { FlatCompat } from "@eslint/eslintrc"
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
 
-const compat = new FlatCompat({
-    // import.meta.dirname is available after Node.js v20.11.0
-    baseDirectory: import.meta.dirname
-})
-
-const eslintConfig = [
-    ...compat.config({
-        extends: ["next/core-web-vitals", "next/typescript", "prettier"]
-    }),
-    {
-        rules: {
-            "@typescript-eslint/no-unused-vars": ["warn"]
-        }
-    }
-]
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts"
+    ])
+])
 
 export default eslintConfig
