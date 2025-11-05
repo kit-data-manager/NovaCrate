9af35a51352cffca34f2fa0677fa33efee520403
Christopher Raquet <xf3778@kit.edu>
2025-11-05 17:21:51 +0100

    WIP: start conceptual work on info page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
 "use client"
 
-import Link from "next/link"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { Button } from "@/components/ui/button"
+import { RotatingText } from "@/components/ui/rotating-text"
 
 export default function Home() {
     const router = useRouter()
 
-    useEffect(() => {
-        router.push("/editor")
-    }, [router])
-
-    return <Link href={"/editor"}>Open Editor</Link>
+    return (
+        <div>
+            <h1 className="text-6xl font-extrabold">NovaCrate</h1>
+            <h2 className="text-2xl flex items-center gap-2">
+                Web-based interactive editor for{" "}
+                <RotatingText text={["creating", "editing", "visualizing", "validating"]} />
+                research object crates.
+            </h2>
+            <Button>Open NovaCrate</Button>
+        </div>
+    )
 }
