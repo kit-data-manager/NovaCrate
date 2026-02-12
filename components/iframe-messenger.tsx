1b7cda731315d02f6bd8ac8f14500d30edf769f5
Christopher Raquet <xf3778@kit.edu>
2026-02-12 17:31:57 +0100

    WIP: work on iframe messenger

--- components/iframe-messenger.tsx
+++ components/iframe-messenger.tsx
@@ -1,31 +1,36 @@
-import { useParams } from "next/navigation"
-import { useCallback, useContext, useEffect, useState } from "react"
+import { useCallback, useContext, useEffect, useRef, useState } from "react"
 import { z } from "zod/mini"
 import { CrateDataContext } from "@/components/providers/crate-data-provider"
+import packageJson from "@/package.json"
 
 const incomingMessageSchema = z.xor([
     z.object({
+        target: z.literal("novacrate"),
         type: z.literal("PUSH_CRATE"),
         metadata: z.optional(z.string())
     }),
     z.object({
+        target: z.literal("novacrate"),
         type: z.literal("PULL_CRATE")
     })
 ])
 
 const outgoingMessageSchema = z.xor([
     z.object({
+        source: z.literal("novacrate"),
         type: z.literal("READY"),
         novaCrateVersion: z.string(),
         messageInterfaceVersion: z.number()
     }),
     z.object({
+        source: z.literal("novacrate"),
         type: z.literal("PULL_CRATE_RESPONSE"),
         metadata: z.optional(z.string())
     })
 ])
 
 type NovaCrateMessageIncoming = z.infer<typeof incomingMessageSchema>
+type NovaCrateMessageOutgoing = z.infer<typeof outgoingMessageSchema>
 
 export function IFrameMessenger() {
     const crateData = useContext(CrateDataContext)
@@ -49,6 +54,7 @@ export function IFrameMessenger() {
         [crateData]
     )
 
+    const hasSentReadyMessage = useRef(false)
     useEffect(() => {
         const messageListener = async (e: MessageEvent) => {
             console.log("Received message", e)
@@ -64,8 +70,21 @@ export function IFrameMessenger() {
         }
 
         window.addEventListener("message", messageListener)
+
+        if (!hasSentReadyMessage.current)
+            window.parent.postMessage(
+                {
+                    source: "novacrate",
+                    type: "READY",
+                    novaCrateVersion: packageJson.version,
+                    messageInterfaceVersion: 1
+                } satisfies NovaCrateMessageOutgoing,
+                "*" // TODO use appropriate origin
+            )
+        hasSentReadyMessage.current = true
+
         return () => window.removeEventListener("message", messageListener)
-    }, [crateData, loadCrate])
+    }, [loadCrate])
 
     return null
 }
