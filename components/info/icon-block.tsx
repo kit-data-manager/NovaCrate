9766395d50bd23c20c47eb527c06bec38b3395ad
Christopher Raquet <xf3778@kit.edu>
2026-04-17 13:49:45 +0200

    fix: make feature overview buttons smoother

--- components/info/icon-block.tsx
+++ components/info/icon-block.tsx
@@ -1,6 +1,7 @@
-import { PropsWithChildren } from "react"
+import React, { PropsWithChildren, useCallback } from "react"
 import { LucideIcon } from "lucide-react"
 import Link from "next/link"
+import { cn } from "@/lib/utils"
 
 export function IconBlock({
     Icon,
@@ -12,10 +13,27 @@ export function IconBlock({
 }>) {
     const Element = href ? Link : "div"
 
+    const anchorHandler = useCallback(
+        (e: React.MouseEvent<HTMLAnchorElement | HTMLDivElement>) => {
+            if (href && href.startsWith("#")) {
+                e.preventDefault()
+                const element = document.getElementById(href.substring(1))
+                if (element) element.scrollIntoView({ behavior: "smooth", block: "start" })
+            }
+        },
+        [href]
+    )
+
     return (
         <Element
             href={href!}
-            className="bg-white dark:bg-black flex drop-shadow-md dark:drop-shadow-none flex-col items-center justify-center lg:gap-6 gap-4 p-4 border border-border rounded-lg lg:w-48 lg:h-40 w-30 h-30"
+            onClick={anchorHandler}
+            className={cn(
+                "bg-white dark:bg-black flex drop-shadow-md dark:drop-shadow-none flex-col items-center justify-center lg:gap-6 gap-4 p-4 border border-border rounded-lg lg:w-48 lg:h-40 w-30 h-30 transition-all",
+                Element !== "div"
+                    ? "hover:drop-shadow-xl hover:border-foreground transition-all"
+                    : ""
+            )}
         >
             <div />
             <div>
