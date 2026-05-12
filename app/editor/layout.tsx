8057adb6f31b113017ec84c05ce423957e199d50
Christopher Raquet <xf3778@kit.edu>
2026-05-12 16:54:12 +0200

    fix: eagerly determine correct persistence provider
    Signed-off-by: Christopher Raquet <xf3778@kit.edu>

--- app/editor/layout.tsx
+++ app/editor/layout.tsx
@@ -3,6 +3,7 @@
 import { PropsWithChildren } from "react"
 import { PersistenceProvider } from "@/components/providers/persistence-provider"
 import { GlobalModalProvider } from "@/components/providers/global-modals-provider"
+import { usePathname } from "next/navigation"
 
 /**
  * Root layout for all `/editor/*` routes.
@@ -16,8 +17,12 @@ import { GlobalModalProvider } from "@/components/providers/global-modals-provid
  * editor routes.
  */
 export default function EditorLayout(props: PropsWithChildren) {
+    const path = usePathname()
+    const extract = /.*\/editor\/([^\/]+)/gm.exec(path)
+    const mode = extract && extract?.length > 1 ? extract[1] : undefined
+
     return (
-        <PersistenceProvider>
+        <PersistenceProvider defaultMode={mode}>
             <GlobalModalProvider>{props.children}</GlobalModalProvider>
         </PersistenceProvider>
     )
